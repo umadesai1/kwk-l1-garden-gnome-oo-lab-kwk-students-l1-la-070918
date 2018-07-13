9a5c3061cf0551d8e27842fde84ffcd71373d12b
Uma Desai <umadesai38@gmail.com>
2018-07-13 16:19:54 +0000

    automatically backed up by learn

--- garden_gnome.rb
+++ garden_gnome.rb
@@ -3,7 +3,7 @@ class GardenGnome
   attr_accessor :name, :age, :gluten_allergy, :hat_color
   
   attr_reader :personality, :gnaw, :shout, :introduce_self
-  def initiazlize(personality = "evil", hat_color = "red")
+  def initialize(personality = "evil", hat_color = "red")
     @personality = "evil"
     @hat_color = hat_color
   end
